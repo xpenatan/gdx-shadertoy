6a9c47fb28e9c3dab058a1ad3bd21d08813dd933
Natan <xpenatan@gmail.com>
2017-02-18 23:28:54 -0300

    updating to last dragome code

--- dragome/src/com/xoppa/JsConfiguration.java
+++ dragome/src/com/xoppa/JsConfiguration.java
@@ -1,6 +1,9 @@
 package com.xoppa;
 
+import java.io.File;
+
 import com.badlogic.gdx.backends.dragome.DragomeConfiguration;
+import com.badlogic.gdx.utils.Array;
 import com.dragome.commons.DragomeConfiguratorImplementor;
 
 @DragomeConfiguratorImplementor(priority = 11)
@@ -9,16 +12,48 @@ public class JsConfiguration extends DragomeConfiguration{
 	@Override
 	public boolean projectClassPathFilter(String projectPath) {
 		boolean include = false;
+		include |= projectPath.contains("/UI/bin");
 		include |= projectPath.contains("ui.jar") || projectPath.contains("ui\\bin") || projectPath.contains("vis-");
 		include |= projectPath.contains("shadertoy");
+		if(projectPath.contains("resources"))
+			include = false;
 		return include;
 	}
-	
+
+	@Override
+	public boolean skipAssetCopy() {
+		return false;
+	}
+
 	@Override
 	public boolean isRemoveUnusedCode() {
-		// True will make proguard use addicitonal-code-keep to reduce javascript size. 
+		// True will make proguard use addicitonal-code-keep to reduce javascript size.
 		// Got to be carefull with the configuration because it can rip out needed class !!!
-		return false; // Dont work now. Require some adjustments 
+		return false; // Dont work now. Require some adjustments
 	}
-	
+
+	@Override
+	public void assetsPath(Array<File> paths) {
+		boolean flag = new File("." + File.separatorChar + "webapp").exists();
+		String path = "." + File.separatorChar + "android" + File.separatorChar + "assets" + File.separatorChar;
+		if(flag)
+			path = "." + path;
+		paths.add(new File(path));
+	}
+
+	@Override
+	public void assetsClasspath(Array<String> filePath) {
+		filePath.add("com/kotcrab/vis/ui/i18n/");
+		filePath.add("com/kotcrab/vis/ui/skin/x1/");
+		filePath.add("com/kotcrab/vis/ui/widget/color/internal/");
+	}
+
+	@Override
+	public String getCompiledPath() {
+		String compiledPath = super.getCompiledPath();
+		if(compiledPath == null)
+			compiledPath = new File("").getAbsolutePath() + File.separatorChar + "dragome" + File.separatorChar + "webapp";
+		return compiledPath;
+	}
+
 }
--- dragome/src/com/xoppa/dragome/launcher/ShaderToyLauncher.java
+++ dragome/src/com/xoppa/dragome/launcher/ShaderToyLauncher.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.backends.dragome.DragomeApplication;
 import com.badlogic.gdx.backends.dragome.DragomeApplicationConfiguration;
 import com.badlogic.gdx.backends.dragome.DragomeWindow;
 import com.badlogic.gdx.backends.dragome.preloader.AssetDownloader.AssetLoaderListener;
-import com.badlogic.gdx.backends.dragome.preloader.AssetFilter.AssetType;
+import com.badlogic.gdx.backends.dragome.preloader.AssetType;
 import com.dragome.web.annotations.PageAlias;
 import com.xoppa.gdx.shadertoy.GdxShaderToy;
 
@@ -22,20 +22,20 @@ public class ShaderToyLauncher extends DragomeApplication
 		getPreloader().loadAsset("com/kotcrab/vis/ui/skin/x1/uiskin.json", AssetType.Text, null, new AssetLoaderListener<Object>());
 		getPreloader().loadAsset("com/kotcrab/vis/ui/skin/x1/uiskin.atlas", AssetType.Text, null, new AssetLoaderListener<Object>());
 		getPreloader().loadAsset("com/kotcrab/vis/ui/skin/x1/uiskin.png", AssetType.Image, null, new AssetLoaderListener<Object>());
-		
+
 		getPreloader().loadAsset("com/kotcrab/vis/ui/i18n/ButtonBar.properties", AssetType.Text, null, new AssetLoaderListener<Object>());
 		getPreloader().loadAsset("com/kotcrab/vis/ui/i18n/ColorPicker.properties", AssetType.Text, null, new AssetLoaderListener<Object>());
 		getPreloader().loadAsset("com/kotcrab/vis/ui/i18n/Dialogs.properties", AssetType.Text, null, new AssetLoaderListener<Object>());
 		getPreloader().loadAsset("com/kotcrab/vis/ui/i18n/FileChooser.properties", AssetType.Text, null, new AssetLoaderListener<Object>());
 		getPreloader().loadAsset("com/kotcrab/vis/ui/i18n/TabbedPane.properties", AssetType.Text, null, new AssetLoaderListener<Object>());
-		
+
 		getPreloader().loadAsset("com/kotcrab/vis/ui/widget/color/internal/checkerboard.frag", AssetType.Text, null, new AssetLoaderListener<Object>());
 		getPreloader().loadAsset("com/kotcrab/vis/ui/widget/color/internal/default.vert", AssetType.Text, null, new AssetLoaderListener<Object>());
 		getPreloader().loadAsset("com/kotcrab/vis/ui/widget/color/internal/hsv.frag", AssetType.Text, null, new AssetLoaderListener<Object>());
 		getPreloader().loadAsset("com/kotcrab/vis/ui/widget/color/internal/palette.frag", AssetType.Text, null, new AssetLoaderListener<Object>());
 		getPreloader().loadAsset("com/kotcrab/vis/ui/widget/color/internal/rgb.frag", AssetType.Text, null, new AssetLoaderListener<Object>());
 		getPreloader().loadAsset("com/kotcrab/vis/ui/widget/color/internal/verticalBar.frag", AssetType.Text, null, new AssetLoaderListener<Object>());
-		
+
 		getPreloader().loadAsset("shaders/default.fragment.glsl", AssetType.Text, null, new AssetLoaderListener<Object>());
 		getPreloader().loadAsset("shaders/default.vertex.glsl", AssetType.Text, null, new AssetLoaderListener<Object>());
 		getPreloader().loadAsset("shaders/water.fragment.glsl", AssetType.Text, null, new AssetLoaderListener<Object>());

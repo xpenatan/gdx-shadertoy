007393ca0315c4ca0bcd41fd7ab954ac8dac3123
Natan <xpenatan@gmail.com>
2017-03-09 21:44:15 -0300

    update

--- dragome/src/com/xoppa/JsConfiguration.java
+++ dragome/src/com/xoppa/JsConfiguration.java
@@ -2,12 +2,13 @@ package com.xoppa;
 
 import java.io.File;
 
-import com.badlogic.gdx.backends.dragome.DragomeConfiguration;
+import com.badlogic.gdx.backends.dragome.DragomeGdxConfiguration;
 import com.badlogic.gdx.utils.Array;
 import com.dragome.commons.DragomeConfiguratorImplementor;
+import com.dragome.compiler.utils.Log;
 
 @DragomeConfiguratorImplementor(priority = 11)
-public class JsConfiguration extends DragomeConfiguration{
+public class JsConfiguration extends DragomeGdxConfiguration{
 
 	@Override
 	public boolean projectClassPathFilter(String projectPath) {
@@ -21,15 +22,23 @@ public class JsConfiguration extends DragomeConfiguration{
 	}
 
 	@Override
+	public int filterClassLog () {
+		return 0;
+	}
+
+	@Override
 	public boolean skipAssetCopy() {
 		return false;
 	}
 
 	@Override
 	public boolean isRemoveUnusedCode() {
-		// True will make proguard use addicitonal-code-keep to reduce javascript size.
-		// Got to be carefull with the configuration because it can rip out needed class !!!
-		return false; // Dont work now. Require some adjustments
+		return true;
+	}
+
+	@Override
+	public boolean isObfuscateCode() {
+		return false;
 	}
 
 	@Override
